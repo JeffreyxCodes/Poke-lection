83f577e9218791cb7ea0c0749e51b9edabdc9090
JeffreyX <jeffreyxdc@gmail.com>
2019-04-27 15:24:11 -0400

    Updates

---
+++ precache-manifest.a1a81262ea63382743896347be336d67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "26b5551ba10f013a335e91b98f530d51",
+    "url": "/Poke-lection/static/media/login-bg.26b5551b.png"
+  },
+  {
+    "revision": "1b0d6d5c9059d19f9b25adab7401970c",
+    "url": "/Poke-lection/static/media/grass.1b0d6d5c.png"
+  },
+  {
+    "revision": "0a1458fee6797c2785d8",
+    "url": "/Poke-lection/static/js/runtime~main.69612f2b.js"
+  },
+  {
+    "revision": "9164679609f736e699b6",
+    "url": "/Poke-lection/static/js/main.51d7aea1.chunk.js"
+  },
+  {
+    "revision": "7f001eb89a5119af2e94",
+    "url": "/Poke-lection/static/js/2.e9b9ce46.chunk.js"
+  },
+  {
+    "revision": "9164679609f736e699b6",
+    "url": "/Poke-lection/static/css/main.ed6e4992.chunk.css"
+  },
+  {
+    "revision": "5f48197902cca2439b47526b23aef60a",
+    "url": "/Poke-lection/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Poke-lection/precache-manifest.a1a81262ea63382743896347be336d67.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Poke-lection/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
